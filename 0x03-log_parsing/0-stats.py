737841d467bdc1bb0d0fdd66561e23de51a5cfb0
Vincent Ekechukwu <136026786+vincetoby@users.noreply.github.com>
2024-06-21 04:25:01 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,47 +1,37 @@
 #!/usr/bin/python3
-""" script that reads stdin line by line and computes metrics
-"""
-
+'''Module for log parsing script.'''
 import sys
 
+if __name__ == "__main__":
+    size = [0]
+    codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
-def printStatus(dic, size):
-    """ func that Prints information 
-    """
-    print("File size: {:d}".format(size))
-    for indx in sorted(dic.keys()):
-        if dic[indx] != 0:
-            print("{}: {:d}".format(indx, dic[indx]))
-
-
-# sourcery skip: use-contextlib-suppress
-statusCodes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-               "404": 0, "405": 0, "500": 0}
-
-count = 0
-size = 0
-
-try:
-    for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            printStatus(statusCodes, size)
-
-        stlist = line.split()
-        count += 1
-
+    def check_match(line):
+        '''Checks for regexp match in line.'''
         try:
-            size += int(stlist[-1])
-        except Exception:
+            line = line[:-1]
+            words = line.split(" ")
+            size[0] += int(words[-1])
+            code = int(words[-2])
+            if code in codes:
+                codes[code] += 1
+        except:
             pass
 
-        try:
-            if stlist[-2] in statusCodes:
-                statusCodes[stlist[-2]] += 1
-        except Exception:
-            pass
-    printStatus(statusCodes, size)
-
-
-except KeyboardInterrupt:
-    printStatus(statusCodes, size)
-    raise
+    def print_stats():
+        '''Prints accumulated statistics.'''
+        print("File size: {}".format(size[0]))
+        for k in sorted(codes.keys()):
+            if codes[k]:
+                print("{}: {}".format(k, codes[k]))
+    i = 1
+    try:
+        for line in sys.stdin:
+            check_match(line)
+            if i % 10 == 0:
+                print_stats()
+            i += 1
+    except KeyboardInterrupt:
+        print_stats()
+        raise
+    print_stats()
